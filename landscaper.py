d26c71f2c630b3495f59869129afb1628c614e0c
Gabriel Royce <geroyceus@gmail.com>
2023-02-01 11:14:49 -0500

    001

--- landscaper.py
+++ landscaper.py
@@ -1,9 +1,21 @@
-landscaper = {"teeth": True, "money": 0, "items": []}
+landscaper = {"teeth": True, "money": 0, "items": ["1"], "currently_equipped": "1"}
 
-shop_items = {"1":{"name": "scissors", 
-                   "price": 5}, 
-              "2":{"name": "lawnmower", 
-                   "price": 25}}
+shop_items = {  "1": {"name": "Teeth",
+                    "price": "none",
+                    "income": 1},
+                "2":{"name": "Rusty Scissors", 
+                   "price": 5,
+                   "income": 5}, 
+                "3":{"name": "Old-Timey Push Lawnmower", 
+                   "price": 25,
+                   "income": 50},
+                "4":{"name": "Fancy Battery-Powered Lawnmower",
+                      "price": 250,
+                      "income": 100
+                      },
+                "5":{"name": "Team of Starving Students",
+                      "price": 500,
+                      "income": 1000,} } 
 
 available_items = []
 
@@ -12,35 +24,57 @@ dialogue_choices = {"1":"Press 1 to cut lawns",
                     "3": "Press 3 to view stats",
                     "Q": "Press Q to escape\n", 
                     "X":"Press X to exit the shop\n", }
-
-
+#############################################################################################################
 shop_escape = True
 space_text = " | "
+#############################################################################################################
+def check_win_condition():
+    if(landscaper["currently_equipped"] == "5" and landscaper["money"] >= 1000):
+        print("Congradulations you win!")
+        print(r"""\
 
+                                   ._ o o
+                                   \_`-)|_
+                                ,""       \ 
+                              ,"  ## |   ಠ ಠ. 
+                            ," ##   ,-\__    `.
+                          ,"       /     `--._;)
+                        ,"     ## /
+                      ,"   ##    /
+
+
+                """)
+        return True
+    else:
+        return False
+#############################################################################################################
 def check_stats():
-    print("\n<<-----Stats----->>")
+    print("\n<<-----Stats----->>\n")
     print("Money:  $"+ str(landscaper["money"]))
-    print("Items: ")
+    print("\nItems: ")
     for item in landscaper["items"]:
-        print("--"+item["name"])
+        print("--"+shop_items[item]["name"])
+    print("\nCurrently Equipped: "+shop_items[landscaper["currently_equipped"]]["name"])
     print("")
-
+#############################################################################################################
 def shop_options():
     for key,value in shop_items.items():
         if key not in landscaper["items"]:
             available_items.append(value)
-
+#############################################################################################################
 def pop_shop():
     available_items.clear()
     counter = 1
     shop_options()
+    print("\n<<<<Welcome to the shop!>>>> \n")
+    print("Current cash: $" + str(landscaper["money"]) + "\n")
     print("The items available are: ")
     for option in available_items:
         str_counter = str(counter)
-        print(f"#{str_counter} - " + option["name"] + " " + str(option["price"]))
+        print(f"#{str_counter} - " + option["name"] + " $" + str(option["price"]))
         counter += 1
     print(" ")
-
+#############################################################################################################
 def visit_shop():
         while(shop_escape):
             pop_shop()
@@ -49,17 +83,23 @@ def visit_shop():
                 return
             else:
                 int_item_choice = int(item_choice) -1
-                if(int_item_choice > available_items[int_item_choice]["price"]):
-                    landscaper["items"].append(available_items[int_item_choice])
-                    print(available_items[int_item_choice]["name"] + " added to inventory!")
+                if(landscaper["money"] >= available_items[int_item_choice]["price"]):
+                    landscaper["money"] = landscaper["money"] - available_items[int_item_choice]["price"]
+                    for key,value in shop_items.items():
+                        if (value["name"] == available_items[int_item_choice]["name"]):
+                            landscaper["items"].append(str(key))
+                            landscaper["currently_equipped"] = str(key)
+                    print(available_items[int_item_choice]["name"] + " equipped!")
                     print()
+                    return
                 else:
                     print("\nNot enough money!\n")
-
+#############################################################################################################
 def check_choice(choice, has_money):
     if(choice == "1"):
-        print("\n!!! You have cut the lawn       +$1\n")
-        landscaper["money"] += 1
+        print("\n!!! You have cut the lawn       +$"+str(shop_items[landscaper["currently_equipped"]]["income"])+"\n")
+        landscaper["money"] += shop_items[landscaper["currently_equipped"]]["income"]
+        print("Current cash: $" + str(landscaper["money"]) + "\n")
     elif(choice == "2" and has_money == True):
         visit_shop()
     elif(choice == "3"):
@@ -68,10 +108,11 @@ def check_choice(choice, has_money):
     else:
         print("\nPlease pick one of the options\n")
         return
-
-
+#############################################################################################################
 while(True):
     has_money = False
+    if(check_win_condition() == True):
+        break
     if(landscaper["money"] >= 5):
         has_money = True
         choice = input(dialogue_choices["1"] + space_text + dialogue_choices["2"] + space_text +dialogue_choices["3"]+space_text+ dialogue_choices["Q"] ).capitalize()
@@ -84,5 +125,3 @@ while(True):
             print("Thanks for playing!")
             break
         check_choice(choice, has_money)
-    
-
